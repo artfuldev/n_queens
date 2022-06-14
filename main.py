d50e91b09dd7c8e6838a955636ffefe3370c6a13
swhustla <fdkelly@gmail.com>
2022-06-14 15:07:34 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,87 @@
+
+# board of size n x n
+# n = number of queens and also size of board
+
+
+from random import choice
+
+
+def remove_queen(board, row, col):
+    board[row][col] = 0
+
+def place_queen(board, row, col):
+    board[row][col] = 1
+
+def print_board(board):
+    for row in board:
+        print(row)
+
+def is_valid_row(board, row):
+    count = 0
+    for col in range(len(board)):
+        count += board[row][col]
+        if count > 1:
+            return False
+    return True
+
+def is_valid_col(board, col):
+    count = 0
+    for row in range(len(board)):
+        count += board[row][col]
+        if count > 1:
+            return False
+    return True
+
+def is_valid_diag(board, row, col):
+    count_left = 0
+    count_right = 0
+    for i in range(len(board)):
+        if row + i < len(board) and col + i < len(board):
+            count_left += board[row + i][col + i]
+            if count_left > 1:
+                return False
+        if row - i >= 0 and col + i < len(board):
+            count_right += board[row - i][col + i]
+            if count_right > 1:
+                return False
+    return True
+
+def is_valid_board(board):
+    for row in range(len(board)):
+        for col in range(len(board)):
+            if board[row][col] == 1:
+                if not is_valid_row(board, row) or  not is_valid_col(board, col) or  not is_valid_diag(board, row, col):
+                    return False
+    return True
+
+def solve(n):
+    board = [[0 for i in range(n)] for j in range(n)]
+
+    for row in range(n):
+        for col in range(n):
+            place_queen(board, row, col)
+            if is_valid_board(board):
+                continue
+            else:
+                remove_queen(board, row, col)
+        
+
+    print("The solved board is:")
+    print_board(board)
+
+
+def main():
+    n = int(input("Enter the size of the board: "))
+    solve(n)
+
+# board = [[0 for i in range(4)] for j in range(4)]
+# board[0][2] = 1
+# board[1][0] = 1
+# board[2][3] = 1
+# board[3][1] = 1
+
+# print(is_valid_row(board, 0))
+# print(is_valid_col(board, 0))
+# print(is_valid_diag(board, 0, 2))
+# print(is_valid_board(board))
+solve(8)
