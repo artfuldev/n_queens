8fe95bd43084a3c06cfe887718da656b38143905
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-06-22 17:47:32 +0100

    Simplify printing

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from timeit import default_timer
 from brute_force_solver import solve_first
-from board import print_board
+from board import sprint_board
 
 for size in range(8, 9):
     tic = default_timer()
     first_solution = solve_first(size)
     toc = default_timer()
     print("1 solution for {}x{} board found in {:0.3f}s".format(size, size, toc - tic))
-    print_board(first_solution)
+    print(sprint_board(first_solution))
