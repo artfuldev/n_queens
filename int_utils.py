3ccb7913eb74a0f492200199fdfff195eaf8d589
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-06-23 11:46:00 +0100

    Rename

--- int_utils.py
+++ int_utils.py
@@ -1,13 +1,13 @@
-__mod_100_nth = {
+__nth_100 = {
     11: 'th',
     12: 'th',
     13: 'th',
 }
-__nth = {
+__nth_10 = {
     1: 'st',
     2: 'nd',
     3: 'rd',
 }
 
 def nth(n: int):
-    return '{}{}'.format(n, __mod_100_nth.get(n % 100, __nth.get(n % 10, 'th')))
+    return '{}{}'.format(n, __nth_100.get(n % 100, __nth_10.get(n % 10, 'th')))
