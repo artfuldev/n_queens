9a320ee5944d0b1e96902fe94d969c133a0d28df
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-06-23 10:28:44 +0100

    Add type annotations

--- board.py
+++ board.py
@@ -3,14 +3,14 @@ from board_utils import colliding_indices, empty, filled, place_queen
 
 class Board:
     @classmethod
-    def empty(cls, size):
+    def empty(cls, size: int):
         return Board(empty(size))
 
     @classmethod
-    def random(cls, size):
+    def random(cls, size: int):
         return Board(filled(size))
 
-    def __init__(self, rows):
+    def __init__(self, rows: list[int | None]):
         self.__size = len(rows)
         self.__rows = rows
         self.__colliding_indices = colliding_indices(self.__rows, self.__size)
@@ -24,10 +24,10 @@ class Board:
     def is_valid(self):
         return self.collisions() == 0
 
-    def queen(self, row):
+    def queen(self, row: int):
         return self.__rows[row]
 
-    def __stringify_row(self, row):
+    def __stringify_row(self, row: int):
         col_strings = []
         for i in range(self.__size):
             col_strings.append("Q" if i == row else "_")
@@ -39,5 +39,5 @@ class Board:
             row_strings.append(self.__stringify_row(row))
         return "\n".join(row_strings) + "\n"
 
-    def place(self, row, col):
+    def place(self, row: int, col: int):
         return Board(place_queen(self.__rows, row, col))
--- board_utils.py
+++ board_utils.py
@@ -2,18 +2,18 @@ from copy import copy
 from random import shuffle
 
 
-def place_queen(board, row, col):
+def place_queen(board: list[int | None], row: int, col: int):
     new_board = copy(board)
     new_board[row] = col
     return new_board
 
 
-def empty(size):
+def empty(size: int):
     return [None for row in range(size)]
 
 
-def filled(size):
-    board = [None for row in range(size)]
+def filled(size: int):
+    board: list[int] = [None for row in range(size)]
     positions = list(range(size))
     shuffle(positions)
     for row in range(size):
@@ -21,7 +21,7 @@ def filled(size):
     return board
 
 
-def has_collision(board, x, y):
+def has_collision(board: list[int | None], x: int, y: int):
     x_queen = board[x]
     y_queen = board[y]
     if x_queen is None or y_queen is None:
@@ -33,8 +33,8 @@ def has_collision(board, x, y):
     return row_diff == col_diff or row_diff == -col_diff
 
 
-def colliding_indices(board, size):
-    indices = []
+def colliding_indices(board: list[int | None], size: int):
+    indices: list[list[int]] = []
     for x in range(size):
         for y in range(size):
             if x != y and has_collision(board, x, y):
@@ -42,7 +42,7 @@ def colliding_indices(board, size):
     return indices
 
 
-def swap(board, x, y):
+def swap(board: list[int | None], x: int, y: int):
     with_new_x = place_queen(board, x, board[y])
     with_new_y = place_queen(with_new_x, y, board[x])
     return with_new_y
