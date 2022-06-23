063c935829f132a62fe679c65022393aaa05bd72
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-06-23 11:22:14 +0100

    Use generators

--- brute_force_solver.py
+++ brute_force_solver.py
@@ -1,41 +1,19 @@
 from board import Board
+from typing import Generator
 
 
-def solve_row(board: Board, row: int):
-    solutions: list[Board] = []
+def __solve(board: Board, row: int) -> Generator[Board, None, None]:
     board_size = board.size()
     if row >= board_size:
-        solutions.append(board)
-        return solutions
-    col = 0
-    while col < board_size:
-        board = board.place(row, col)
-        if board.is_valid_until(row):
-            for solution in solve_row(board, row + 1):
-                solutions.append(solution)
-        col += 1
-    return solutions
+        yield board
+    else:
+        col = 0
+        while col < board_size:
+            board = board.place(row, col)
+            if board.is_valid_until(row):
+                for solution in __solve(board, row + 1):
+                    yield solution
+            col += 1
 
-
-def solve_row_first(board: Board, row: int):
-    board_size = board.size()
-    solution: Board | None = None
-    if row >= board_size:
-        return board
-    col = 0
-    while col < board_size:
-        board = board.place(row, col)
-        if board.is_valid_until(row):
-            solution = solve_row_first(board, row + 1)
-            if solution is not None:
-                return solution
-        col += 1
-    return solution
-
-
-def solve(size):
-    return solve_row(Board(size), 0)
-
-
-def solve_first(size):
-    return solve_row_first(Board(size), 0)
+def solve(size: int):
+    return __solve(Board(size), 0)
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from timeit import default_timer
-from brute_force_solver import solve_first
+from brute_force_solver import solve
 
 for size in range(15, 16):
     tic = default_timer()
-    first_solution = solve_first(size)
+    solution = next(solve(size), None)
     toc = default_timer()
     seconds = toc - tic
-    if first_solution is not None:
+    if solution is not None:
         print("1 solution for {}x{} board found in {:0.3f}s".format(size, size, seconds))
-        print(first_solution.stringify())
+        print(solution.stringify())
     else:
         print("No solution found for {}x{} board in {:0.3f}s".format(size, size, seconds))
