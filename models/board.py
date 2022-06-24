3b7ab5702d1b346afc84302353ea3b7de68aa624
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-06-24 07:48:29 +0100

    Simplify

--- models/board.py
+++ models/board.py
@@ -22,18 +22,12 @@ def indices(size: int) -> Generator[Index, None, None]:
             if x != y:
                 yield [x, y]
 
-
-def __stringify_row(size: int, row: int):
-    col_strings = []
-    for i in range(size):
-        col_strings.append("Q" if i == row else "_")
-    return " ".join(col_strings)
-
-
 def stringify(board: Board):
     size = len(board)
     row_strings = []
     for row in board:
-        row_strings.append(__stringify_row(size, row))
-        row_strings.append("\n")
+        col_strings = []
+        for i in range(size):
+            col_strings.append("Q" if i == row else "_")
+        row_strings.append("{}\n".format(" ".join(col_strings)))
     return "".join(row_strings)
