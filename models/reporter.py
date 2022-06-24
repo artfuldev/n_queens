b0e0397ce289b70dd473fab2793355684005a941
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-06-24 14:26:47 +0100

    Simplify

--- models/reporter.py
+++ models/reporter.py
@@ -1,28 +1,14 @@
 from typing import Callable
 from models.board import Board
 
-Reporter = Callable[[int, int, list[Board]], str]
-
-summary = "size {:3d}, {:3d} solution{}, {:0.3f}s, {:20s}, {}"
+summary = "size: {:3d}, solutions: {:5d}, time: {:8.3f}s, method: {:20s}, last: {}"
 
 
 def report(size: int, seconds: int, name: str, solutions: list[Board]) -> str:
-    solutions_count = len(solutions)
-    if solutions_count != 0:
-        return summary.format(
-            size,
-            solutions_count,
-            "s" if solutions_count != 1 else "",
-            seconds,
-            name,
-            solutions[-1],
-        )
-    else:
-        return summary.format(
-            size,
-            0,
-            "s",
-            seconds,
-            name,
-            None,
-        )
+    return summary.format(
+        size,
+        len(solutions),
+        seconds,
+        name.rjust(20),
+        solutions[-1] if any(solutions) else None,
+    )
