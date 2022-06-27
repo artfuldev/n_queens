d6b56864bf5a925339fc741bef56c7140fe904fb
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-06-27 19:53:58 +0100

    Simplify

--- algorithms/back_tracking.py
+++ algorithms/back_tracking.py
@@ -4,29 +4,33 @@ Candidate = TypeVar("Candidate")
 Problem = TypeVar("Problem")
 Solution = TypeVar("Solution")
 
+def __identity(_: Problem, candidate: Candidate):
+    return candidate
 
 def back_tracking(
-    root: Callable[[Problem], Candidate],
+    root: Callable[[Problem], Candidate | None],
     reject: Callable[[Problem, Candidate], bool],
     accept: Callable[[Problem, Candidate], bool],
-    output: Callable[[Problem, Candidate], Solution],
     first: Callable[[Problem, Candidate], Candidate | None],
     next: Callable[[Problem, Candidate], Candidate | None],
+    output: Callable[[Problem, Candidate], Solution] = __identity,
 ):
     def try_solve(
         problem: Problem, candidate: Candidate
     ) -> Generator[Solution, None, None]:
+        if reject(problem, candidate):
+            return
         if accept(problem, candidate):
             yield output(problem, candidate)
             return
         extension = first(problem, candidate)
         while extension is not None:
-            if not reject(problem, extension):
-                for solution in try_solve(problem, extension):
-                    yield solution
+            for solution in try_solve(problem, extension):
+                yield solution
             extension = next(problem, extension)
 
     def solve(problem: Problem):
-        return try_solve(problem, root(problem))
+        candidate = root(problem)
+        return None if candidate is None else try_solve(problem, candidate)
 
     return solve
--- solutions/back_tracking.py
+++ solutions/back_tracking.py
@@ -1,86 +1,63 @@
+from copy import copy
 from functools import partial
-from itertools import islice
-from typing import Generator, NewType, Tuple
+from typing import Generator
 from models.board import (
     Board,
     RowPair,
     Size,
-    Row,
     Column,
     has_collision,
     row_pairs,
-    place_queen,
 )
 from algorithms.back_tracking import back_tracking as algorithm
 
-Candidate = NewType("Candidate", Tuple[Row, Column, Board])
+
+def __root(_: Size) -> Board | None:
+    return Board([])
 
 
-def __row(candidate: Candidate) -> Row:
-    row, *_ = candidate
-    return row
-
-
-def __column(candidate: Candidate) -> Column:
-    _, column, *_ = candidate
-    return column
-
-
-def __board(candidate: Candidate) -> Board:
-    *_, board = candidate
-    return board
-
-
-def __root(size: Size) -> Candidate:
-    return Candidate((Row(-1), Column(0), Board([Column(0) for _ in range(size)])))
-
-
-def __indices(size: Size, candidate: Candidate) -> Generator[RowPair, None, None]:
-    row = __row(candidate)
+def __indices(size: Size, board: Board) -> Generator[RowPair, None, None]:
+    filled_size = len(board)
     for pair in row_pairs(size):
         x, y = pair
-        if x <= row and y <= row:
+        if x < filled_size and y < filled_size:
             yield pair
 
 
-def __reject(size: Size, candidate: Candidate) -> bool:
-    reject = any(
-        filter(partial(has_collision, __board(candidate)), __indices(size, candidate))
-    )
-    return reject
+def __reject(size: Size, board: Board) -> bool:
+    return any(filter(partial(has_collision, board), __indices(size, board)))
 
 
-def __accept(size: Size, candidate: Candidate) -> bool:
-    accept = __row(candidate) == size - 1
-    return accept
+def __accept(size: Size, board: Board) -> bool:
+    return len(board) == size
 
 
-def __first(size: Size, candidate: Candidate) -> Candidate:
-    row = Row(__row(candidate) + 1)
-    column = Column(0)
-    board = place_queen(__board(candidate), row, column)
-    while len(set(islice(board, row + 1))) != row + 1:
+def __concat(board: Board, col: Column) -> Board:
+    cols = copy(board)
+    cols.append(col)
+    return cols
+
+
+def __replace_last(board: Board, col: Column) -> Board:
+    cols = copy(board)
+    cols[-1] = col
+    return cols
+
+
+def __next(size: Size, board: Board) -> Board | None:
+    filled = len(board)
+    if filled > size:
+        return None
+    column = Column(board[-1] + 1)
+    board = __replace_last(board, column)
+    while len(set(board)) != filled and column < size:
         column = Column(column + 1)
-        board = place_queen(__board(candidate), row, column)
-    first = Candidate((row, column, board))
-    return first
+        board = __replace_last(board, column)
+    return None if column == size else board
 
 
-def __next(size: Size, candidate: Candidate) -> Candidate | None:
-    row = __row(candidate)
-    column = Column(__column(candidate) + 1)
-    next = None
-    board = place_queen(__board(candidate), row, column)
-    while len(set(islice(board, row + 1))) != row + 1 and column != size:
-        column = Column(column + 1)
-        board = place_queen(__board(candidate), row, column)
-    if column != size:
-        next = Candidate((row, column, board))
-    return next
+def __first(size: Size, board: Board) -> Board | None:
+    return __next(size, __concat(board, Column(-1)))
 
 
-def __output(size: Size, candidate: Candidate):
-    return __board(candidate)
-
-
-back_tracking = algorithm(__root, __reject, __accept, __output, __first, __next)
+back_tracking = algorithm(__root, __reject, __accept, __first, __next)
