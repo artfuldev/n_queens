31c66af3b2f66cf3958661f7d84104b6f35ea292
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-06-27 19:26:39 +0100

    Add an optional output

--- algorithms/brute_force.py
+++ algorithms/brute_force.py
@@ -2,18 +2,22 @@ from typing import Callable, Generator, TypeVar
 
 Candidate = TypeVar("Candidate")
 Problem = TypeVar("Problem")
+Solution = TypeVar("Solution")
 
+def __identity(_: Problem, candidate: Candidate):
+    return candidate
 
 def brute_force(
     first: Callable[[Problem], Candidate | None],
     next: Callable[[Problem, Candidate], Candidate | None],
     accept: Callable[[Problem, Candidate], bool],
-) -> Callable[[Problem], Generator[Candidate, None, None]]:
+    output: Callable[[Problem, Candidate], Solution] = __identity
+) -> Callable[[Problem], Generator[Solution, None, None]]:
     def solve(problem: Problem):
         candidate = first(problem)
         while candidate is not None:
             if accept(problem, candidate):
-                yield candidate
+                yield output(problem, candidate)
             candidate = next(problem, candidate)
 
     return solve
