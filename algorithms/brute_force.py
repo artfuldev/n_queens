bbe2537ce68f4175161160d50b30a9f820bd8699
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-06-27 20:17:57 +0100

    Fix formatting

--- algorithms/brute_force.py
+++ algorithms/brute_force.py
@@ -4,14 +4,16 @@ Candidate = TypeVar("Candidate")
 Problem = TypeVar("Problem")
 Solution = TypeVar("Solution")
 
+
 def __identity(_: Problem, candidate: Candidate):
     return candidate
+
 
 def brute_force(
     first: Callable[[Problem], Candidate | None],
     next: Callable[[Problem, Candidate], Candidate | None],
     accept: Callable[[Problem, Candidate], bool],
-    output: Callable[[Problem, Candidate], Solution] = __identity
+    output: Callable[[Problem, Candidate], Solution] = __identity,
 ) -> Callable[[Problem], Generator[Solution, None, None]]:
     def solve(problem: Problem):
         candidate = first(problem)
