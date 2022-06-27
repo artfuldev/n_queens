142687406963c492e6bca526408b5af005194898
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-06-27 20:07:05 +0100

    Simplify

--- solutions/back_tracking.py
+++ solutions/back_tracking.py
@@ -3,10 +3,12 @@ from functools import partial
 from typing import Generator
 from models.board import (
     Board,
+    Row,
     RowPair,
     Size,
     Column,
     has_collision,
+    place_queen,
     row_pairs,
 )
 from algorithms.back_tracking import back_tracking as algorithm
@@ -16,48 +18,36 @@ def __root(_: Size) -> Board | None:
     return Board([])
 
 
-def __indices(size: Size, board: Board) -> Generator[RowPair, None, None]:
+def __reject(size: Size, board: Board) -> bool:
     filled_size = len(board)
     for pair in row_pairs(size):
         x, y = pair
-        if x < filled_size and y < filled_size:
-            yield pair
-
-
-def __reject(size: Size, board: Board) -> bool:
-    return any(filter(partial(has_collision, board), __indices(size, board)))
+        if x < filled_size and y < filled_size and has_collision(board, pair):
+            return True
+    return False
 
 
 def __accept(size: Size, board: Board) -> bool:
     return len(board) == size
 
 
-def __concat(board: Board, col: Column) -> Board:
-    cols = copy(board)
-    cols.append(col)
-    return cols
-
-
-def __replace_last(board: Board, col: Column) -> Board:
-    cols = copy(board)
-    cols[-1] = col
-    return cols
-
-
 def __next(size: Size, board: Board) -> Board | None:
     filled = len(board)
     if filled > size:
         return None
+    row = Row(filled - 1)
     column = Column(board[-1] + 1)
-    board = __replace_last(board, column)
+    board = place_queen(board, row, column)
     while len(set(board)) != filled and column < size:
         column = Column(column + 1)
-        board = __replace_last(board, column)
+        board = place_queen(board, row, column)
     return None if column == size else board
 
 
 def __first(size: Size, board: Board) -> Board | None:
-    return __next(size, __concat(board, Column(-1)))
+    seed = copy(board)
+    seed.append(Column(-1))
+    return __next(size, seed)
 
 
 back_tracking = algorithm(__root, __reject, __accept, __first, __next)
