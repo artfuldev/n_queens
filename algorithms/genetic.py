189c7c058ddbeccccb5ffc93e8330d7740af8345
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-07-04 11:40:40 +0100

    Simplify

--- algorithms/genetic.py
+++ algorithms/genetic.py
@@ -1,54 +1,93 @@
 from dataclasses import dataclass
-from typing import Callable, Generator, Generic, Tuple, TypeVar
+from random import choices
+from typing import (
+    Callable,
+    Generator,
+    Generic,
+    Tuple,
+    TypeAlias,
+    TypeVar,
+    cast,
+)
 
 
 Candidate = TypeVar("Candidate")
 Problem = TypeVar("Problem")
 Solution = TypeVar("Solution")
 
+C: TypeAlias = Candidate
+P: TypeAlias = Problem
+S: TypeAlias = Solution
 
-@dataclass
+
+@dataclass(frozen=True)
 class Individual(Generic[Candidate]):
     candidate: Candidate
     fitness: float
 
 
+def individual_candidate(individual: Individual[C]):
+    return individual.candidate
+
+
+def individual_fitness(individual: Individual[C]) -> float:
+    return individual.fitness
+
+
+def __select(_: P, population: list[Individual[C]]) -> Tuple[C, C]:
+    return cast(
+        Tuple[C, C],
+        list(
+            map(
+                individual_candidate,
+                choices(
+                    population, weights=list(map(individual_fitness, population)), k=2
+                ),
+            )
+        ),
+    )
+
+
+@dataclass(frozen=True)
+class Result(Individual[Candidate]):
+    generations: int
+
+
+def __result(_: P, individual: Individual[C], generations: int) -> Result[C]:
+    return Result(individual.candidate, individual.fitness, generations)
+
+
 def genetic(
-    population: Callable[[Problem], list[Candidate]],
-    fitness: Callable[[Problem, Candidate], float],
-    selection: Callable[
-        [Problem, list[Individual[Candidate]]], Tuple[Candidate, Candidate]
-    ],
-    crossover: Callable[[Candidate, Candidate], Candidate],
-    mutate: Callable[[Candidate], Candidate],
-    accept: Callable[[Problem, Candidate], bool],
-    output: Callable[
-        [Problem, Candidate], Solution
-    ] = lambda problem, candidate: candidate,
-) -> Callable[[Problem], Solution]:
-    def solve(problem: Problem):
-        def solve(
-            problem: Problem, population: list[Candidate]
-        ) -> Generator[Solution, None, None]:
+    population: Callable[[P], list[C]],
+    fitness: Callable[[P, C], float],
+    crossover: Callable[[P, C, C], C],
+    mutate: Callable[[P, C], C],
+    terminate: Callable[[P, Individual[C], int], bool],
+    select: Callable[[P, list[Individual[C]]], Tuple[C, C]] = __select,
+    output: Callable[[P, Individual[C], int], S] = __result,
+) -> Callable[[P], Generator[S, None, None]]:
+    def solve(problem: P) -> Generator[S, None, None]:
+        def solve(problem: P, population: list[C], generation: int) -> S:
             population_count = len(population)
             gene_pool = list(
                 map(lambda c: Individual(c, fitness(problem, c)), population)
             )
             while True:
                 for individual in gene_pool:
-                    if accept(problem, individual.candidate):
-                        yield output(problem, individual.candidate)
-                        return
+                    if terminate(problem, individual, generation):
+                        return output(problem, individual, generation)
                 next_gene_pool = []
                 for i in range(population_count):
-                    parent_1, parent_2 = selection(problem, gene_pool)
-                    child = crossover(parent_1, parent_2)
-                    mutated = mutate(child)
+                    parent_1, parent_2 = select(problem, gene_pool)
+                    child = crossover(problem, parent_1, parent_2)
+                    mutated = mutate(problem, child)
                     next_gene_pool.append(
                         Individual(mutated, fitness(problem, mutated))
                     )
                 gene_pool = next_gene_pool
+                generation += 1
 
-        return solve(problem, population(problem))
+        while True:
+            yield solve(problem, population(problem), 1)
 
     return solve
