735639e10e52b2711cfed69eb09c60a381559204
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-07-19 09:57:22 +0100

    Improve particle swarm algorithm

--- algorithms/particle_swarm.py
+++ algorithms/particle_swarm.py
@@ -1,38 +1,106 @@
-from functools import partial
-from typing import Callable, List, Tuple, TypeAlias, TypeVar
+from dataclasses import dataclass
+from random import uniform
+from typing import Callable, List, NewType, TypeVar
 from algorithms.solve import Solve
 
-Candidate = TypeVar("Candidate")
 Problem = TypeVar("Problem")
 Solution = TypeVar("Solution")
+Position = NewType("Position", List[float])
+Velocity = NewType("Velocity", List[float])
 
-Position: TypeAlias = Tuple[int, ...]
-Magnitude: TypeAlias = float
-Direction: TypeAlias = Tuple[int, ...]
-Velocity: TypeAlias = Tuple[Magnitude, Direction]
-Particle: TypeAlias = Tuple[Position, Velocity]
-Swarm: TypeAlias = List[Particle]
-State: TypeAlias = Tuple[Swarm, Position]
-Step: TypeAlias = int
+
+@dataclass
+class Particle:
+    position: Position
+    velocity: Velocity
+    best: Position
+
+
+@dataclass
+class Swarm:
+    particles: List[Particle]
+    best: Position
+
+
+@dataclass(frozen=True)
+class Range:
+    low: float
+    high: float
+
+
+def __move(limits: list[Range], position: Position, velocity: Velocity) -> Position:
+    next_position = []
+    for d in range(len(limits)):
+        limit = limits[d]
+        next_value = position[d] + velocity[d]
+        while (next_value > limit.high) or (next_value < limit.low):
+            if next_value > limit.high:
+                next_value = (2 * limit.high) - next_value
+            if next_value < limit.low:
+                next_value = (2 * limit.low) - next_value
+        next_position.append(next_value)
+    return Position(next_position)
+
+
+def __initialize(
+    swarm_size: int, limits: List[Range], quality: Callable[[Position], float]
+) -> Swarm:
+    dimensions = list(range(len(limits)))
+    swarm = Swarm([], Position(list(map(lambda r: uniform(r.low, r.high), limits))))
+    for _ in range(swarm_size):
+        position = Position([])
+        velocity = Velocity([])
+        for dimension in dimensions:
+            low = limits[dimension].low
+            high = limits[dimension].high
+            position.append(uniform(low, high))
+            abs_range = abs(high - low)
+            velocity.append(uniform(-abs_range, abs_range))
+        particle = Particle(position, velocity, position)
+        swarm.particles.append(particle)
+        if quality(position) > quality(swarm.best):
+            swarm.best = position
+    return swarm
+
 
 def particle_swarm(
-    candidates: Callable[[Problem], List[Candidate]],
-    velocity: Callable[[Problem, Step], Magnitude],
-    down: Callable[[Problem, Candidate], Position],
-    up: Callable[[Problem, Position], Candidate],
-    quality: Callable[[Problem, Candidate], float],
-    steps: Callable[[Problem], int],
+    size: Callable[[Problem], int],
+    ranges: Callable[[Problem], List[Range]],
+    quality: Callable[[Problem, Position], float],
+    terminate: Callable[[Problem, Position], bool],
+    velocity: Callable[[Problem, Particle, Position], Velocity],
+    output: Callable[[Problem, Position], Solution],
 ) -> Solve[Problem, Solution]:
     def solve(problem: Problem):
-        step = 0
-        magnitude = velocity(problem, step)
-        swarm_candidates = candidates(problem)
-        global_best = down(problem, (max(swarm_candidates, key=lambda c: quality(problem, c))))
-        swarm = []
-        for candidate in swarm_candidates:
-            position = down(problem, candidate)
-            particle = [position, ]
-            swarm.append(particle)
-        pass
-    
+        def solve(problem: Problem):
+            _size = size(problem)
+            _limits = ranges(problem)
+
+            def _quality(position: Position) -> float:
+                return quality(problem, position)
+
+            swarm = __initialize(_size, _limits, _quality)
+            while not terminate(problem, swarm.best):
+                for i in range(_size):
+                    swarm.particles[i].velocity = velocity(
+                        problem,
+                        swarm.particles[i],
+                        swarm.best,
+                    )
+                    swarm.particles[i].position = __move(
+                        _limits,
+                        swarm.particles[i].position,
+                        swarm.particles[i].velocity,
+                    )
+                    if _quality(swarm.particles[i].position) > _quality(
+                        swarm.particles[i].best
+                    ):
+                        swarm.particles[i].best = swarm.particles[i].position
+                        if _quality(swarm.particles[i].best) > _quality(swarm.best):
+                            swarm.best = swarm.particles[i].best
+            return output(problem, swarm.best)
+
+        while True:
+            yield solve(problem)
+
     return solve
