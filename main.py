af7e2049cfd7dd07092d827ae98172864028daaf
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-07-20 14:45:17 +0100

    Reuse n

--- main.py
+++ main.py
@@ -21,14 +21,15 @@ def find_solutions(size: int, count=1, until: int = None):
     if count <= 0:
         raise
     stop = size + 1 if until is None else until + 1
-    for n in range(size, stop):
+    for i in range(size, stop):
+        n = Size(i)
         n_count = count
         for algorithm, solve in __solvers.items():
             started = default_timer()
-            solutions = list(islice(solve(Size(n)), n_count))
+            solutions = list(islice(solve(n), n_count))
             n_count = len(solutions)
             ended = default_timer()
-            yield Report(algorithm, Size(n), solutions, ended - started)
+            yield Report(algorithm, n, solutions, ended - started)
 
 
 for report in find_solutions(4, until=8):
