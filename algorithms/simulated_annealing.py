d9a3200ad2ef0d0ffe4bee8bfc34aa9f2e0b8154
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-07-25 15:56:01 +0100

    Simplify

--- algorithms/simulated_annealing.py
+++ algorithms/simulated_annealing.py
@@ -1,6 +1,5 @@
-from dataclasses import dataclass
-from random import uniform
-from typing import Generic, TypeVar, Callable, Any
+from random import choices
+from typing import TypeVar, Callable, Any
 
 from .solve import Solve
 from .from_optimizer import from_optimizer
@@ -15,16 +14,6 @@ NeighborEnergy = Energy
 Probability = float
 
 
-@dataclass(frozen=True)
-class State(Generic[System]):
-    system: System
-    energy: Energy
-
-
-def __create_state(system: System, energy: Callable[[System], Energy]) -> State[System]:
-    return State(system, energy(system))
-
-
 def __always(problem: Any, system: Any):
     return True
 
@@ -35,26 +24,25 @@ def anneal(
     neighbor: Callable[[Problem, System], System],
     temperature: Callable[[Problem, RemainingBudget], Temperature],
     energy: Callable[[Problem, System], Energy],
-    terminate: Callable[[Problem, Energy], bool],
-    probability: Callable[[Problem, Energy, NeighborEnergy, Temperature], Probability],
+    terminate: Callable[[Problem, System], bool],
+    accept: Callable[[Problem, Energy, NeighborEnergy, Temperature], Probability],
     key: Callable[[Problem, System], str],
     valid: Callable[[Problem, System], bool] = __always,
 ) -> Solve[Problem, System]:
     def optimize(problem: Problem) -> System:
-        def __state(system: System) -> State[System]:
-            return State(system, energy(problem, system))
-        state = __state(first(problem))
+        s = first(problem)
         k_max = budget(problem)
         for k in range(k_max):
-            current_temperature = temperature(problem, 1 - ((k + 1) / k_max))
-            if terminate(problem, state.energy):
-                return state.system
-            state_next = __state(neighbor(problem, state.system))
-            acceptance_probability = probability(
-                problem, state.energy, state_next.energy, current_temperature
+            if terminate(problem, s):
+                return s
+            s_new = neighbor(problem, s)
+            p = accept(
+                problem,
+                energy(problem, s),
+                energy(problem, s_new),
+                temperature(problem, 1 - ((k + 1) / k_max)),
             )
-            if acceptance_probability > uniform(0, 1):
-                state = state_next
-        return state.system
+            s = choices([s, s_new], [1 - p, p], k=1)[0]
+        return s
 
     return from_optimizer(key, valid, optimize)
